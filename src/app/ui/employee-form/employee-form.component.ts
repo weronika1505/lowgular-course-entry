96dfb434979b136d8b784cd9eea7d250b7448408
weronika1505 <weronika.pacholec1505@gmail.com>
2022-10-02 21:01:15 +0200

    display status

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -10,7 +10,7 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
   name: new FormControl(null, [Validators.required]),
-  age: new FormControl(null,[Validators.min(18)]),
+  age: new FormControl(null,[Validators.min(18), Validators.required]),
   salary: new FormControl(null, [Validators.required])
 });
 }
