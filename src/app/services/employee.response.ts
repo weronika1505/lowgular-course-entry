4334cd30644f02b27138b821f066c718cae8473c
weronika1505 <weronika.pacholec1505@gmail.com>
2022-10-17 22:29:56 +0200

    map response to model - exercise 5

---
+++ src/app/services/employee.response.ts
@@ -0,0 +1,7 @@
+export interface EmployeeResponse {
+  readonly id: string;
+  readonly employee_name: string;
+  readonly employee_salary: string;
+  readonly employee_age: string;
+  readonly profile_image: "";
+}
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,14 +4,31 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PeopleModel } from '../model/people.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
+import {EmployeeModel} from "../model/employee.model";
+
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
   getAll(): Observable<PeopleModel[]> {
-    return this._httpClient.get<PeopleModel[]>("assets/data/people.json")
-  }
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees')
+      .pipe(
+        map((response: ApiResponse<EmployeeResponse[]>): PeopleModel[] => {
+          return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+          name: employeeResponse.employee_name,
+          personalNumber: employeeResponse.id,
+          img: employeeResponse.profile_image,
+          mail: employeeResponse.employee_name + '@lowgular.io',
+          };
+          });
+        })
+      );
+}
+
   create(employee: CreateEmployeeModel): Observable<void> {
     return this._httpClient.post('https://dummy.restapiexample.com/', employee).pipe(map(_ => void 0));
   }
